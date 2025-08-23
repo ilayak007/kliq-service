d42d595e6fc6b698bd1989e3e4c18927566af7d1
ilayak <ilayaraja.kasirajan@clubhub.ai>
2025-08-23 17:59:50 +0400

    check for time while submitting the prediction

--- src/controllers/predictionController.js
+++ src/controllers/predictionController.js
@@ -1,3 +1,4 @@
+const moment = require('moment-timezone');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
@@ -5,6 +6,31 @@ const createPrediction = async (req, res) => {
   const { matchId, customerId, customerName, customerSelected, result, pointsEarned, isPointsUpdated } = req.body;
 
   try {
+
+    // START 
+    const currentIST = moment().tz('Asia/Kolkata');
+    const todayISTDate = currentIST.format('YYYY-MM-DD');
+
+    const match = await prisma.matches.findFirst({
+      where: { matchId: matchId },
+    });
+
+    if (!match) {
+      return res.status(404).json({ error: "Match not found" });
+    }
+
+    const matchIST = moment.utc(match.matchStartDateTime).tz('Asia/Kolkata');
+    const matchDate = matchIST.format('YYYY-MM-DD');
+    const diffInMinutes = matchIST.diff(currentIST, 'minutes');
+
+        // if either condition fails → return 400
+        if (matchDate !== todayISTDate || diffInMinutes < 60) {
+          return res.status(400).json({
+            error: "Your prediction could not be submitted because the cutoff time has passed."
+          });
+        }
+    // FINISH
+
     const prediction = await prisma.customerPrediction.create({
       data: {
         matchId,
